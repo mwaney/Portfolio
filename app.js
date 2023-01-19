9d23598c3ecb79a8cf8ea16bde534882d2ef8c7a
Edwin Mwaniki <emwaniki91@gmail.com>
2023-01-19 19:27:59 +0300

    Link contact form to googlesheets and display a success message when one succeeds in sending info

--- app.js
+++ app.js
@@ -20,3 +20,21 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = "-200px";
 }
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxUBpuNGYWNriNOVbG2RN4jOVQ2chsMPbYmFQ-bBrihnGFRbucRtrDYI4WuTNeDVMqs/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent Successfully!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
