1e083f932b24b016d2fcbbd6e9385d5792dc0839
Edwin Mwaniki <emwaniki91@gmail.com>
2023-01-19 18:31:50 +0300

    Create footer and make the website responsive on small devices

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ function openTab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+let sideMenu = document.getElementById("sideMenu");
+
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
