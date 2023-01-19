655e76cac9e495d177f333feb73ff61a4c700976
Edwin Mwaniki <emwaniki91@gmail.com>
2023-01-19 10:47:21 +0300

    Create a structure of the website, with nav, header and about me sections and style it with some css

---
+++ app.js
@@ -0,0 +1,13 @@
+let tablinks = document.querySelectorAll(".tab-links");
+let tabcontents = document.querySelectorAll(".tab-contents");
+
+function openTab(tabname) {
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
